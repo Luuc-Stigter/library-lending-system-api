9cb8e82f21bc02075087d7740aed3cec0d82b0b8
Luuc Stigter <Luuc.Stigter@novi-education.nl>
2025-05-09 01:38:15 +0200

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/luucstigter/library_lending_system_api/LibraryLendingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.luucstigter.library_lending_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryLendingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryLendingSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luucstigter/library_lending_system_api/LibraryLendingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luucstigter.library_lending_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryLendingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
