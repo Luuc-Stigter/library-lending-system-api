b047d36d4d5098da8fe5c32d5ff62cbfc29cf839
Luuc Stigter <Luuc.Stigter@novi-education.nl>
2025-06-18 17:48:13 +0200

    feat(dto): Create UserDto and UserInputDto

---
+++ src/main/java/com/luucstigter/library_lending_system_api/dto/UserDto.java
@@ -0,0 +1,7 @@
+package com.luucstigter.library_lending_system_api.dto;
+
+public class UserDto {
+    public String username;
+    public String email;
+    public String role;
+}
---
+++ src/main/java/com/luucstigter/library_lending_system_api/dto/UserInputDto.java
@@ -0,0 +1,8 @@
+package com.luucstigter.library_lending_system_api.dto;
+
+public class UserInputDto {
+    public String username;
+    public String password;
+    public String email;
+    public String role;
+}
