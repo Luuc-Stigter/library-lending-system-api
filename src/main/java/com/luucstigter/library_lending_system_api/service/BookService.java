9281d296df6c4571156754741032739779378480
Luuc Stigter <Luuc.Stigter@novi-education.nl>
2025-06-18 15:58:31 +0200

    feat(service): Implement full CRUD logic in BookService

--- src/main/java/com/luucstigter/library_lending_system_api/service/BookService.java
+++ src/main/java/com/luucstigter/library_lending_system_api/service/BookService.java
@@ -1,11 +1,13 @@
 package com.luucstigter.library_lending_system_api.service;
 
+import com.luucstigter.library_lending_system_api.exception.ResourceNotFoundException;
 import com.luucstigter.library_lending_system_api.model.Book;
 import com.luucstigter.library_lending_system_api.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -22,7 +24,31 @@ public class BookService {
     }
 
     public Book addBook(Book book) {
-        // TODO later validatie toevoegen, bv. controleren of ISBN al bestaat.
         return bookRepository.save(book);
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Boek niet gevonden met id: " + id));
+    }
+
+    public Book updateBook(Long id, Book bookDetails) {
+        Book book = getBookById(id);
+
+        book.setTitle(bookDetails.getTitle());
+        book.setAuthor(bookDetails.getAuthor());
+        book.setIsbn(bookDetails.getIsbn());
+        book.setPublisher(bookDetails.getPublisher());
+        book.setPublicationYear(bookDetails.getPublicationYear());
+        book.setGenre(bookDetails.getGenre());
+
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        if (!bookRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Boek niet gevonden met id: " + id);
+        }
+        bookRepository.deleteById(id);
     }
 }
